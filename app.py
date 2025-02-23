611359c89f9612b22e29298aef81c7e377d72f30
guy <guymeltzer28@gmail.com>
2025-02-23 19:58:28 +0200

    .

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_discover():
                     break  # stop searching after the first 20 items
 
     return jsonify(results)
-
+#
 
 @app.route('/updatePopularity', methods=['POST'])
 def update_popularity():
